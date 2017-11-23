350e311d0ea699c45d96760001e1f0240dfc1e74
paas <paas@cse-codegen-backend-4193087237-2br1h>
2017-11-23 01:46:02 +0000

    Auto pushed by ServiceStage!

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM 100.125.0.198:20202/hwcse/dockerhub-java:8-jre-alpine
 
 WORKDIR /home/apps/
-ADD target/test007-1.0.0.jar .
+ADD target/ggggg-1.0.0.jar .
 ADD target/lib ./lib
 ADD start.sh .
 
---
+++ src/main/java/com/service/ggggg/controller/RaozhengguangDelegate.java
@@ -0,0 +1,14 @@
+package com.service.ggggg.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class RaozhengguangDelegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}
